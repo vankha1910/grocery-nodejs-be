c98167ab532d19cad5c4c3c2cae21ab76074d1df
vankha19 <vankha2000@gmail.com>
2024-10-09 00:16:00 +0700

    fix cors error

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'http://https://grocery-market-react.vercel.app/'
+    'https://grocery-market-react.vercel.app'
   ); // Replace with your frontend origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
